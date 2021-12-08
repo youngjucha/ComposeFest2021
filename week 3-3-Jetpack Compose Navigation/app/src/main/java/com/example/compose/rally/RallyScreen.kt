52d9c4c1415efbcda83ebf9cc5839a06dfe91f0a
youngju <youngjucha0316@gmail.com>
2021-12-08 21:45:34 +0900

    week 3-3 Jetpack Compose Navigation

--- week 3-3-Jetpack Compose Navigation/app/src/main/java/com/example/compose/rally/RallyScreen.kt
+++ week 3-3-Jetpack Compose Navigation/app/src/main/java/com/example/compose/rally/RallyScreen.kt
@@ -31,26 +31,18 @@ import com.example.compose.rally.ui.overview.OverviewBody
  * Screen metadata for Rally.
  */
 enum class RallyScreen(
-    val icon: ImageVector,
-    val body: @Composable ((String) -> Unit) -> Unit
+    val icon: ImageVector
 ) {
     Overview(
         icon = Icons.Filled.PieChart,
-        body = { OverviewBody() }
     ),
     Accounts(
         icon = Icons.Filled.AttachMoney,
-        body = { AccountsBody(UserData.accounts) }
     ),
     Bills(
         icon = Icons.Filled.MoneyOff,
-        body = { BillsBody(UserData.bills) }
     );
 
-    @Composable
-    fun content(onScreenChange: (String) -> Unit) {
-        body(onScreenChange)
-    }
 
     companion object {
         fun fromRoute(route: String?): RallyScreen =
